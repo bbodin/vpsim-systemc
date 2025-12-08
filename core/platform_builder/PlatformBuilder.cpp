bf36c1ce516c77a45312e3d72bed2e4682c454b3
Bruno Bodin <bbodin@acm.org>
2025-12-08 15:34:48 +0100

    peanut

--- core/platform_builder/PlatformBuilder.cpp
+++ core/platform_builder/PlatformBuilder.cpp
@@ -23,7 +23,7 @@ namespace vpsim {
     int PlatformBuilder::Container = 0;
 
     PlatformBuilder::PlatformBuilder(std::string platformName) : mCurrentIp(nullptr) {
-        LOG_GLOBAL_DEBUG(dbg0) << "PlatformBuilder cosntructor called." << std::endl;
+        LOG_GLOBAL_DEBUG(dbg0) << "PlatformBuilder constructor called." << std::endl;
         if (platformName == "")
             platformName = to_string(Container++);
         mCurrentIp = &beginBuild("Container", platformName);
